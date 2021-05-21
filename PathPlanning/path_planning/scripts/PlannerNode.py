5ab188e2853982196fac6fc148726552a3c40e22
Prateek Mukherjee <75921678+P-1702@users.noreply.github.com>
2021-05-21 15:10:03 +0530

    Update PlannerNode.py

--- PathPlanning/path_planning/scripts/PlannerNode.py
+++ PathPlanning/path_planning/scripts/PlannerNode.py
@@ -155,18 +155,18 @@ class PlannerNode:
         path = astar_search(start, end)
         count=0
         for move in path:
-        count+=1
-        if count<len(path):
-            x=move[0]
-            y=move[1]
-            x1=(path[count])[0]
-            y1=(path[count])[1]
-            direc=predict_move(x,y,x1,y1)
-            next_mov = direction()
-            next_mov.direction=direc
-            self.direction_publisher.publish(next_mov)
-        else:
-            pass    
+            count+=1
+            if count<len(path):
+                        x=move[0]
+                        y=move[1]
+                        x1=(path[count])[0]
+                        y1=(path[count])[1]
+                        direc=predict_move(x,y,x1,y1)
+                        next_mov = direction()
+                        next_mov.direction=direc
+                        self.direction_publisher.publish(next_mov)
+           else:
+                        pass    
         print('Steps to goal: {0}'.format(len(path)))
 
     # This class represents a node
@@ -174,21 +174,6 @@ class PlannerNode:
 
 
         
-    
-
- 
-
-
-
-
-
-        
-            
-            
-            
-
-
-
 
 
 if __name__ == '__main__':
